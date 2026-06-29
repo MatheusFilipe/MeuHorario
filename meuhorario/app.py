d069c3dac6a1c0de055ebc6d5215a98d29372dc2
Matheus Filipe Rodrigues <matheus.filipes.rodrigues@gmail.com>
2026-06-29 14:41:12 -0300

    fix(api): implement CORS middleware to prevent origin errors

--- meuhorario/app.py
+++ meuhorario/app.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from meuhorario.routers import appointments, auth, services, users
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://matheusfilipe.github.io',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(services.router)
